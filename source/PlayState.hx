89d4f67f41d7b1293cf31f853669d0e20df74c97
Hexa2239 <yourlocalhexa@proton.me>
2024-09-15 15:15:18 +0100

    Beta Test VER 0.0.1

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,14 +4,38 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.util.FlxColor;
+import sprites.Number;
 
 class PlayState extends FlxState
 {
+	var number = 0;
+
 	override public function create()
 	{
+		FlxG.mouse.visible = false;
+
 		var bg = new FlxSprite();
 		bg.makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE);
 		add(bg);
+
 		super.create();
+	}
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (FlxG.mouse.justPressed)
+		{
+			createNumber();
+		}
+	}
+
+	public function createNumber()
+	{
+		number += 1;
+		var num:Number = new Number(number);
+		// num.x = FlxG.mouse.x;
+		// num.y = FlxG.mouse.y;
+		add(num);
 	}
 }
---
+++ source/sprites/Number.hx
@@ -0,0 +1,54 @@
+package sprites;
+
+import flixel.FlxG;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+
+class Number extends FlxText
+{
+	public var beginWreckSequence = 0;
+
+	public function new(curNumber:Int)
+	{
+		super();
+		trace("Creating number instance with num: " + curNumber);
+		font = "assets/fonts/bubble.ttf";
+		// color = FlxColor.BLACK; (Was just testing placing sys)
+		color = FlxColor.fromRGB(FlxG.random.int(10, 240), FlxG.random.int(10, 240), FlxG.random.int(10, 240)); // Prevent full white and prevent full black.
+		text = Std.string(curNumber);
+		size = 50;
+
+		x = FlxG.mouse.x;
+		y = FlxG.mouse.y;
+
+		new FlxTimer().start(3, function(timer:FlxTimer)
+		{
+			beginWreckSequence = 1;
+		});
+	}
+
+	override public function update(elapsed)
+	{
+		if (beginWreckSequence == 1)
+		{
+			if (alpha > 0)
+			{
+				alpha -= 0.005;
+			}
+			else
+			{
+				destroy();
+				return;
+			}
+		}
+
+		super.update(elapsed);
+	}
+
+	override public function destroy()
+	{
+		trace("[Number Instance] I'm finished! Bye bye!");
+		super.destroy();
+	}
+}
