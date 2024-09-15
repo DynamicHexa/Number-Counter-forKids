917d02ef7c03e6c666e5067bcb8f046d6f15b1f5
Hexa2239 <yourlocalhexa@proton.me>
2024-09-15 15:00:00 +0100

    Git ignore and bg

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,17 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
 	override public function create()
 	{
+		var bg = new FlxSprite();
+		bg.makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE);
+		add(bg);
 		super.create();
-	}
-
-	override public function update(elapsed:Float)
-	{
-		super.update(elapsed);
 	}
 }
